d1f6765e5f7d27eac5b2fde95e280db198ab850a
Monadios <gfalkenthros@gmail.com>
2015-11-05 09:08:46 +0100

    Cleaned up the code a bit

--- school.py
+++ school.py
@@ -1,4 +1,5 @@
 from bs4 import BeautifulSoup
+from time import sleep
 import urllib.request
 import json
 
@@ -19,17 +20,15 @@ def get_schools(url):
     
 
 baseurl = 'http://www.lectio.dk/'
+
 studenturl = 'http://www.lectio.dk/lectio/557/SkemaNy.aspx?type=elev&elevid='
-schools = get_schools('skole.html')
 
+def make_school_tuple():                               
+    for school in schools:                             
+        schooltuple.append(                            
+            (''.join(school.contents[0].splitlines()), 
+             school.get('href')))                      
 
-##########################################################
-# def make_school_tuple():                               #
-#     for school in schools:                             #
-#         schooltuple.append(                            #
-#             (''.join(school.contents[0].splitlines()), #
-#              school.get('href')))                      #
-##########################################################
 
 
 def parse_studentID(info):
@@ -37,15 +36,15 @@ def parse_studentID(info):
     return sid
 
 def get_student(school_id, letter):
-    url = '%slectio/%/FindSkema.aspx?type=elev&forbogstav=%s' % baseurl,
-    school_id, letter
+    url = baseurl + 'lectio/' + school_id + '/FindSkema.aspx?type=elev&forbogstav=' + letter
     link = urllib.request.urlopen(url).read()
     soup = BeautifulSoup(link, 'html.parser')
     students = []
 
-    for x in soup.find_all('ul')[1].find_all('li'):
-        students.append((x.find('a').contents[0],
-    parse_studentID(x.find('a').get('href'))))
+    for x in soup.find_all('ul')[1].find_all('li'): 
+        students.append((x.find('a').contents[0],   
+                         parse_studentID(x.find('a').get('href'))))      
+    
 
     return students
     
@@ -54,8 +53,17 @@ def dump_schools(file_name):
         for school in get_schools('skole.html'):
             f.write(school[0] + ',' + school[1] + '\n')
 
+
 if __name__ == '__main__':
-    charset = [list(range(ord('a'), ord('z'))), 230, 229, 248]
-#    doc = urllib.request.urlopen(
- #       'https://www.lectio.dk/lectio/557/FindSkema.aspx?type=elev').read()
-  #  soup = BeautifulSoup(doc, 'html.parser')
+    schoolnames = {}
+    schooldict = {}
+    schools = get_schools('skole.html')[179]
+    charset = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N']
+
+    for school in [schools]:
+        schoolnames[school[1]] = school[0]
+    
+    for school in [schools]:                                                              
+        for letter in charset:                                                          
+            schooldict[letter] = get_student(school[1], letter)
+            sleep(1)
