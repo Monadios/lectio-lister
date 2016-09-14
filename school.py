d5d74e69b10cdb29a291913f1372fe514b4dc738
Monadios <gfalkenthros@gmail.com>
2016-09-14 15:56:57 +0200

    Fledged out features and cleared up the structure

--- school.py
+++ school.py
@@ -1,5 +1,5 @@
 from bs4 import BeautifulSoup
-nnnfrom time import sleep
+from time import sleep
 import urllib.request
 from random import randrange
 from string import ascii_uppercase
@@ -13,7 +13,7 @@ class School:
     
     def make_url(self, letter):
         return 'http://www.lectio.dk/' + 'lectio/' + self.id + '/FindSkema.aspx?type=elev&forbogstav=' + letter
-        
+
     def letter_range(self, f,l,al = ascii_uppercase):
         for x in al[al.index(f):al.index(l)+1]:
             yield x
@@ -31,9 +31,9 @@ class School:
                 student_link = s.find('a').get('href')
                 student = Student(student_name, student_link)
                 students.append(student)
+    
+        self.students = students
         
-            self.students = students
-            
 
 class Student:
     def __init__(self, name, id):
@@ -41,27 +41,28 @@ class Student:
         self.name = self.tokens[0]
         self.class_info = self.tokens[1]
         self.id = id
-        
+         
     def parse_name(self, n):
         class_info = n[n.find("(")+1:n.find(")")]
         name = n.split("(")[0]
         return name, class_info
-    
-    def get_schools(url):
-        doc = open(url).read()
-        soup = BeautifulSoup(doc, 'html.parser')
-        schools = soup.find_all('a')
-    
-        schoollist = []
 
-        for school in schools:
-            schoolname = school.contents[0].replace('\n','')
-            ID = school.get('href').replace("/lectio/", "").replace("/default.aspx", "")
-            schoollist.append((schoolname, ID))
-            print("Downloaded " + schoolname + "\n")
-            sleep(randrange(5,15))
-            
-        return schoollist
+def get_schools(url):
+    doc = open(url).read()
+
+    soup = BeautifulSoup(doc, 'html.parser')
+    schools = soup.find_all('a')
+
+    schoollist = []
+
+    for school in schools:
+        schoolname = school.contents[0].replace('\n','')
+        ID = school.get('href').replace("/lectio/", "").replace("/default.aspx", "")
+        schoollist.append((schoolname, ID))
+        print("Downloaded " + schoolname + "\n")
+        sleep(randrange(5,15))
+
+    return schoollist
 
 
 def main():
